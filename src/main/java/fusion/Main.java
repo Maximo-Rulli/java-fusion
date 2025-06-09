837d78426b31c5ba6dfcb4fe0092c8d837b23636
Maximo-Rulli <rullimaximoeduardo@gmail.com>
2025-06-09 11:22:33 +0200

    cleaned main and created tests file

--- src/main/java/fusion/Main.java
+++ src/main/java/fusion/Main.java
@@ -2,46 +2,9 @@ package fusion;
 
 import java.io.IOException;
 
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
-
 public class Main {
   public static void main(String[] args) throws IOException, InterruptedException {
-    INDArray th = (Nd4j.create(new float[]{2})).broadcast(1,1,28,28);
-    //System.out.println("Boradcasted " + t.broadcast(1,1,28,28));
-
-    INDArray a = Nd4j.create(new float[]{
-        1, 2, 3, 4, 5}, new int[]{1, 5});
-
-    //INDArray image = a.broadcast(3, 2, 4, 4);
-
-    INDArray k = Nd4j.create(new float[]{
-                            1, 0, 0, 0, 0,
-                            0, 1, 0, 0, 0,
-                            0, 0, 1, 0, 0,
-                            0, 0, 0, 1, 0,
-                            0, 0, 0, 0, 1,
-                            1, 0, 1, 0, 1,
-                          
-                          }, new int[]{6,5});
-    
-    INDArray b = Nd4j.create(new float[]{1}, new int[]{1});
-    //Layers.maxPool(a, 2, 2);
-
-    //System.out.println(Layers.concat(a, a));
-
-    Layer TestL = new Layer(k, b);
-
-    INDArray testB = Nd4j.rand(new int[]{1, 2, 4, 4});
-
-    //System.out.println(TestL.Linear(a, true));
-
     Correctness.run();
-    //Layers.Conv(b, 1, 1, 2, 1, 0);
-
-    UNet Model = new UNet();
-
-    System.out.println(Model.predict(th, b));
-    
+    Tests.run();
   }
 }
---
+++ src/main/java/fusion/Tests.java
@@ -0,0 +1,45 @@
+package fusion;
+
+import java.io.IOException;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+public class Tests {
+  public static void run() {
+    INDArray th = (Nd4j.create(new float[]{2})).broadcast(1,1,28,28);
+    //System.out.println("Boradcasted " + t.broadcast(1,1,28,28));
+
+    INDArray a = Nd4j.create(new float[]{
+        1, 2, 3, 4, 5}, new int[]{1, 5});
+
+    //INDArray image = a.broadcast(3, 2, 4, 4);
+
+    INDArray k = Nd4j.create(new float[]{
+                            1, 0, 0, 0, 0,
+                            0, 1, 0, 0, 0,
+                            0, 0, 1, 0, 0,
+                            0, 0, 0, 1, 0,
+                            0, 0, 0, 0, 1,
+                            1, 0, 1, 0, 1,
+                          
+                          }, new int[]{6,5});
+    
+    INDArray b = Nd4j.create(new float[]{1}, new int[]{1});
+    //Layers.maxPool(a, 2, 2);
+
+    //System.out.println(Layers.concat(a, a));
+
+    Layer TestL = new Layer(k, b);
+
+    INDArray testB = Nd4j.rand(new int[]{1, 2, 4, 4});
+
+    //System.out.println(TestL.Linear(a, true));
+
+    //Layers.Conv(b, 1, 1, 2, 1, 0);
+
+    UNet Model = new UNet();
+
+    System.out.println(Model.predict(th, b));
+  }
+}
